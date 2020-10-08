4d5054e87abc84f90b1ed9991a43f952686208ff
Sangha Lee <tkdgklee@gmail.com>
2020-10-08 11:43:06 -0400

    reset

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Sangha Lee on 10/6/20.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
